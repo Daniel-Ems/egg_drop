77c7043b8d7ba2a9dec4f077cadc4b63c1854d08
Daniel <danielems3@outlook.com>
2017-01-20 13:03:08 -0500

    Add the quadratic formula and work to add  a binary search and then implement with input

--- egg_drop.c
+++ egg_drop.c
@@ -3,9 +3,11 @@
 #include <sysexits.h>
 #include <stdint.h>
 #include <limits.h>
+
 #include "egg.h"
 
-//bool input_check(char *floors, char *eggs);
+size_t binary(int num_drops, size_t floors);
+double max_drops(long floors);
 int main(int argc, char *argv[])
 {
 	//This is checking to make sure the appropriate numbers of arguments 
@@ -18,7 +20,7 @@ int main(int argc, char *argv[])
 	}
 
 	char *bad;
-	long floors = strtol(argv[1], &bad, 10);
+	size_t floors = strtol(argv[1], &bad, 10);
 	
 	//This is checking to make sure the values passed are good values
 	if(*bad|| floors > UINT_MAX || floors < 1)
@@ -27,28 +29,89 @@ int main(int argc, char *argv[])
 		return EX_USAGE;
 	}
 
-	int egg = strtol(argv[2], &bad, 10);
-	if(*bad || egg > INT_MAX || egg < 1)
+	//This is checking to make sure the egg value is good
+	int num_drops = strtol(argv[2], &bad, 10);
+	if(*bad || num_drops > INT_MAX || num_drops < 1)
 	{
 		printf("Egg Max=INT_MAX\nEgg Min=1\nNo Decimals\nNumbers only\n");
 		return EX_USAGE;
 	}
 	
 	
-	//TODO -> Makefile
-	//TODO -> finish this code
-	//TODO -> figure out how to round the float up to the nearest int
+	size_t half;
+	while(num_drops >= 3)
+	{
+		half = binary(num_drops, floors);
+		printf("%zd", half);
+	}
+	size_t start = max_drops(half);
+	printf("start = %zd\n", start);
+
+	size_t drop = start;
+	int i = 1;
+	size_t prior;
+	egg *test = lay_egg();
+	for(drop = start; drop < floors; i++)
+	{
+		egg_drop_from_floor(test, drop);
+		if(egg_is_broken(test))
+		{
+			printf("Egg broke at %zd", drop);
+			break;
+		}
+		printf("drop %zd\n", drop);
+		prior = drop;
+		drop += start - i;
+	}
+
+	while(egg_is_broken(test))
+	{
+		prior++;
+		egg_drop_from_floor(test,drop);
+	}
+
+	int max_safe = prior -1;
+	printf("max safe %d", max_safe);
+
 	//TODO -> figure out if you have more eggs
 	//TODO -> Command line input and all that good shit. 
-	floors = (floors*8)+1;
-	printf("%ld\n", floors);
-	floors = sqrt(floors);
-	floors = (floors-1)/2;
-	//g -= 1;
-	printf("%ld", floors);
-
-	
 
 }
+
+double max_drops(long floors)
+{
+	double b =1;
+	double a= 1;
+	double c = floors * -2;
+
+	double max_drops = ceil((-b + sqrt(b*b - 4*a*c)) / (2*a));
+	//printf("temp before rounding = %lf\n", );
+
+	//int max_drops = ceil(temp);
+
+	//printf("%d\n", max_drops);
+
+	return max_drops;
+
+}
+
+size_t binary(int num_drops, size_t floors)
+{
+	size_t drop_point = floors;
+	egg *test  = lay_egg();
+	
+	while(test)
+	{
+		drop_point /= 2;
+		egg_drop_from_floor(test, drop_point);
+		size_t temp = drop_point;	
+		if(egg_is_broken(test)
+		{
+			return temp;
+		}
+	}
+}		
+	
+	
 
 
